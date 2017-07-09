edc8cf48e2547c64655e313e6c8ee2fd3c3ddece
YoProgrammer <yuttasakcom@gmail.com>
2017-07-09 15:45:42 +0700

    Create layout index page

--- cmd/gonews/main.go
+++ cmd/gonews/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/yuttasakcom/gonews/pkg/app"
 )
@@ -11,5 +12,25 @@ const port = ":8080"
 func main() {
 	mux := http.NewServeMux()
 	app.Mount(mux)
+	mux.Handle("/-/", http.StripPrefix("/-", http.FileServer(noDir{http.Dir("static")})))
 	http.ListenAndServe(port, mux)
+}
+
+type noDir struct {
+	http.Dir
+}
+
+func (d noDir) Open(name string) (http.File, error) {
+	f, err := d.Dir.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	stat, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+	if stat.IsDir() {
+		return nil, os.ErrNotExist
+	}
+	return f, nil
 }
